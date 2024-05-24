c2e56c4f46dcc65df5baf9730e231634e9f9e19e
aqys <ilikestrawberry69@gmail.com>
2024-05-24 18:53:43 +0200

    p100 changes

--- poophack/Offsets.cs
+++ poophack/Offsets.cs
@@ -11,8 +11,11 @@ namespace poophack
         //bases
         public int localPlayer = 0x180DB18;
         public int entityList = 0x181C1D0;
+        public int viewMatrix = 0x180DB18;
 
         // attibutes
+        public int teamNum = 0x3c3;
+        public int jumpFlag = 0x3cc;
         public int health = 0x324;
         public int origin = 0x0D58;
     }
--- poophack/Program.cs
+++ poophack/Program.cs
@@ -1,19 +1,62 @@
 ﻿using ClickableTransparentOverlay;
 using Swed64;
 using System.Numerics;
+using ImGuiNET;
+using System.Runtime.InteropServices;
 
 namespace poophack
 {
     class Program : Overlay
     {
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
         Swed swed = new Swed("cs2");
         Offsets offsets = new Offsets();
         Entity localPlayer = new Entity();
         List<Entity> entities = new List<Entity>();
+        List<Entity> enemyTeam = new List<Entity>();
+        List<Entity> playerTeam = new List<Entity>();
         IntPtr client;
+
+        // global colors
+        Vector4 teamColor = new Vector4(0, 0, 1, 1); // blue
+        Vector4 enemyColor = new Vector4(1, 0, 0, 1); // red
+        Vector4 healthBarColor = new Vector4(0, 1, 0, 1); // green
+        Vector4 healthTextColor = new Vector4(1, 1, 1, 1); // white
+
+        // screen vars
+        Vector2 windowLocation = new Vector2(0, 0);
+        Vector2 windowSize = new Vector2(1920, 1080);
+        Vector2 lineOrigin = new Vector2(1920 / 2, 1080); // middle of screen
+        Vector2 windowCenter = new Vector2(1920 / 2, 1080 / 2);
+
+        // imgui checkboxes
+        bool enableEsp = true;
+
+        bool enableTeamBox = false;
+        bool enableTeamHealthBar = false;
+        bool enableTeamLine = false;
+        bool enabledTeamDot = false;
+        bool enabledTeamDistance = false;
+
+        bool enableEnemyBox = true;
+        bool enableEnemyHealthBar = true;
+        bool enableEnemyLine = false;
+        bool enabledEnemyDot = false;
+        bool enabledEnemyDistance = false;
 
         protected override void Render()
         {
+            //ImGui.Begin("poophack");
             // render stuff here
         }
 
@@ -23,11 +66,37 @@ namespace poophack
             
             while (true) // always run
             {
-                entities.Clear(); // clear lists
-                localPlayer.address = swed.ReadPointer(client, offsets.localPlayer); // set address so we can update
-                UpdateEntity(localPlayer); // update
+                
+            }
+        }
 
-                Console.WriteLine($"localPlayer health -> {localPlayer.health}");
+        void ReloadEntities()
+        {
+            entities.Clear(); // clear lists
+            localPlayer.address = swed.ReadPointer(client, offsets.localPlayer); // set address so we can update
+            UpdateEntity(localPlayer); // update
+            UpdateEntities(); // update all entities
+        }
+
+        void UpdateEntities() // handle all other entities
+        {
+            for (int i = 0; i < 64; i++) // normally less than 64 entities
+            {
+                IntPtr tempEntityAddress = swed.ReadPointer(client, offsets.entityList + i * 0x08);
+                if(tempEntityAddress == IntPtr.Zero)
+                    continue; // skip if leading to address
+
+                Entity entity = new Entity();
+                entity.address = tempEntityAddress;
+                UpdateEntity(entity);
+
+                if (entity.health < 1 || entity.health > 100)
+                    continue; // check if entity is dead or invalid
+
+                if (!entities.Any(element => element.origin.X == entity.origin.X))
+                {
+                    entities.Add(entity);
+                }
             }
         }
 
