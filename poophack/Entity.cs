849ca77ae880278d4d9675241d05f7a3f0ea3f47
aqys <ilikestrawberry69@gmail.com>
2024-05-24 15:27:05 +0200

    thread logic stuff

---
+++ poophack/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace poophack
+{
+    public class Entity
+    {
+        public IntPtr address { get; set; }
+        public int health { get; set; }
+        public Vector3 origin { get; set; }
+    }
+}
--- poophack/Program.cs
+++ poophack/Program.cs
@@ -6,14 +6,29 @@ namespace poophack
 {
     class Program : Overlay
     {
+        Swed swed = new Swed("cs2.exe");
+        Entity localPlayer = new Entity();
+        List<Entity> entities = new List<Entity>();
+        IntPtr client;
+
         protected override void Render()
         {
             // render stuff here
         }
 
+        void MainLogic()
+        {
+
+        }
+
         static void Main(string[] args)
         {
             // logic methods & stuff here
+            Program program = new Program();
+            program.Start().Wait();
+
+            Thread mainLogicThread = new Thread(program.MainLogic) { IsBackground = true }; // logic thread
+            mainLogicThread.Start();
         }
     }
 }
