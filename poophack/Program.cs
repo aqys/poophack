bbc4ed9273adc6382df8a914d7ff76d7e68685ab
aqys <ilikestrawberry69@gmail.com>
2024-05-24 15:19:16 +0200

    Add project files.

---
+++ poophack/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClickableTransparentOverlay;
+using Swed64;
+using System.Numerics;
+
+namespace poophack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
